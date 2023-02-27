06d679ada46857e4ccdb588e3505cee532e15342
rogueeasy <ezekieladeraw@gmail.com>
2023-02-27 13:57:12 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: pointer to the string to chech
+ * Return: void
+ */
+int _strlen(cher *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
