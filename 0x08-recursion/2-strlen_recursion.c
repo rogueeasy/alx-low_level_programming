cc78fb5af94134ea3016918135802bf4fa1e6b3e
rogueeasy <ezekieladeraw@gmail.com>
2023-03-09 12:44:13 -0800

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,17 +3,14 @@
 /**
  * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
- *
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	s = "lpa\0";
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
-
