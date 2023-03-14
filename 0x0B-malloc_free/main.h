d6e6ef21916be71b8d2dbb11c9ac556ee5dc3a6a
rogueeasy <ezekieladeraw@gmail.com>
2023-03-14 11:12:47 -0700

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,8 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef "main.h"
+#define "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
